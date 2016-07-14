cd8077551add5398dc12f30be4a07d9b4e5d1ce4
Mark Severson <markse@instructure.com>
2016-07-14 12:59:33 -0600

    fix dynamo container to terminate gracefully
    Change-Id: I492c2b3db3571e1ef23e3445ec88c0fa2bd20f9d
    Reviewed-on: https://gerrit.instructure.com/85320
    Reviewed-by: Ken Romney <kromney@instructure.com>
    Product-Review: Ken Romney <kromney@instructure.com>
    QA-Review: Ken Romney <kromney@instructure.com>
    Tested-by: Ken Romney <kromney@instructure.com>

--- appliances/dynamo/entrypoint.sh
+++ appliances/dynamo/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb $@
+exec /usr/bin/java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb $@
