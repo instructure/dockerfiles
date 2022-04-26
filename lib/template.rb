538d82b14230da081437121c989d0b46772bfb64
Aaron Ogata <aogata@instructure.com>
2022-04-26 13:31:11 -0700

    allow base image path to be overridden for base images
    refs DE-1159
    
    Change-Id: Ia231e4d1237a4d6bcad1acfae8f3365422a40205
    Reviewed-on: https://gerrit.instructure.com/c/dockerfiles/+/290518
    Reviewed-by: Alex Slaughter <aslaughter@instructure.com>
    QA-Review: Aaron Ogata <aogata@instructure.com>
    Tested-by: Service Cloud Jenkins <svc.cloudjenkins@instructure.com>

--- lib/template.rb
+++ lib/template.rb
@@ -14,11 +14,37 @@ class Template
   end
 
   def render(values)
-    TemplateRenderer.new(self, OpenStruct.new(values))
+    TemplateRenderer.new(self, TemplateMethods.new(values))
   end
 
   def self.render_into_dockerfile(path, values)
     new(path).render(values).to_string
+  end
+
+  class TemplateMethods
+    def initialize(values)
+      @root_path_included = false
+      @source = OpenStruct.new(values)
+    end
+
+    def method_missing(method, *args, &block)
+      @source.send(method, *args, &block)
+    end
+
+    def from_image(base_image)
+      image_name = base_image.is_a?(Hash) ? base_image['name'] : base_image
+      image_source = base_image.is_a?(Hash) ? base_image['source'] : nil
+
+      use_root_path = image_source != 'dockerhub' && !@root_path_included
+      image_path = use_root_path ? "${ROOT_PATH}/#{image_name}" : image_name
+
+      res = ""
+      res << "ARG ROOT_PATH=instructure\n" if use_root_path
+      res << "FROM #{image_path}"
+
+      @root_path_included ||= true
+      res
+    end
   end
 
   class TemplateRenderer
